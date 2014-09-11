64f1511bb23271127427fcdbcd95ce50cdb0134b
RudolfHenning_cp <SND\RudolfHenning_cp>
2014-09-11 10:55:13 +0000

    Version 3.24 release

--- QuickMon3/QuickMonService/Properties/AssemblyInfo.cs
+++ QuickMon3/QuickMonService/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.24.0909.2014")]
-[assembly: AssemblyFileVersion("3.24.0909.2014")]
+[assembly: AssemblyVersion("3.24.0911.2014")]
+[assembly: AssemblyFileVersion("3.24.0911.2014")]
