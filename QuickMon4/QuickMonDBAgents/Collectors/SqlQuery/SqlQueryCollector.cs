be23bd0951955051dd95d320a357a29f05116554
RudolfHenning_cp <SND\RudolfHenning_cp>
2015-02-06 13:32:33 +0000

    Starting on SQL Query Agent

---
+++ QuickMon4/QuickMonDBAgents/Collectors/SqlQuery/SqlQueryCollector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace QuickMon.Collectors
+{
+    [Description("Sql Query Collector"), Category("Database")]
+    public class SqlQueryCollector : CollectorAgentBase
+    {
+        public SqlQueryCollector()
+        {
+            AgentConfig = new SqlQueryCollectorConfig();
+        }
+
+        public override MonitorState RefreshState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<System.Data.DataTable> GetDetailDataTables()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
