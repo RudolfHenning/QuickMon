f99e71fbd00a661c062dac5f5d68b4fe2437f6e1
RudolfHenning_cp <SND\RudolfHenning_cp>
2015-02-16 11:49:57 +0000

    With All Sql Collectors

---
+++ QuickMon4/QuickMonDBUIForWinForms/Collectors/SqlDatabaseSize/SqlDatabaseSizeCollectorEditEntry.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace QuickMon.Collectors
+{
+    partial class SqlDatabaseSizeCollectorEditEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SqlDatabaseSizeCollectorEditEntry));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.warningNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.errorNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.cmdTest = new System.Windows.Forms.Button();
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.cboDatabase = new System.Windows.Forms.ComboBox();
+            this.lblDatabase = new System.Windows.Forms.Label();
+            this.cmdLoadDBs = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkIntegratedSec = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDownCmndTimeOut = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.warningNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCmndTimeOut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.warningNumericUpDown);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.errorNumericUpDown);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(13, 176);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(513, 64);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Alert triggers";
+            // 
+            // warningNumericUpDown
+            // 
+            this.warningNumericUpDown.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.warningNumericUpDown.Location = new System.Drawing.Point(129, 27);
+            this.warningNumericUpDown.Maximum = new decimal(new int[] {
+            1073741824,
+            0,
+            0,
+            0});
+            this.warningNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.warningNumericUpDown.Name = "warningNumericUpDown";
+            this.warningNumericUpDown.Size = new System.Drawing.Size(75, 20);
+            this.warningNumericUpDown.TabIndex = 1;
+            this.warningNumericUpDown.Value = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.warningNumericUpDown.ValueChanged += new System.EventHandler(this.warningNumericUpDown_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Warning size:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(216, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "MB, Error size:";
+            // 
+            // errorNumericUpDown
+            // 
+            this.errorNumericUpDown.Location = new System.Drawing.Point(302, 26);
+            this.errorNumericUpDown.Maximum = new decimal(new int[] {
+            1073741824,
+            0,
+            0,
+            0});
+            this.errorNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.errorNumericUpDown.Name = "errorNumericUpDown";
+            this.errorNumericUpDown.Size = new System.Drawing.Size(75, 20);
+            this.errorNumericUpDown.TabIndex = 3;
+            this.errorNumericUpDown.Value = new decimal(new int[] {
+            4096,
+            0,
+            0,
+            0});
+            this.errorNumericUpDown.ValueChanged += new System.EventHandler(this.errorNumericUpDown_ValueChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(379, 29);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "MB";
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmdCancel.Location = new System.Drawing.Point(451, 253);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(75, 23);
+            this.cmdCancel.TabIndex = 16;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            // 
+            // cmdTest
+            // 
+            this.cmdTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdTest.Enabled = false;
+            this.cmdTest.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmdTest.Location = new System.Drawing.Point(289, 253);
+            this.cmdTest.Name = "cmdTest";
+            this.cmdTest.Size = new System.Drawing.Size(75, 23);
+            this.cmdTest.TabIndex = 14;
+            this.cmdTest.Text = "Test";
+            this.cmdTest.UseVisualStyleBackColor = true;
+            this.cmdTest.Click += new System.EventHandler(this.cmdTest_Click);
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdOK.Enabled = false;
+            this.cmdOK.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmdOK.Location = new System.Drawing.Point(370, 253);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(75, 23);
+            this.cmdOK.TabIndex = 15;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
+            // 
+            // cboDatabase
+            // 
+            this.cboDatabase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboDatabase.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cboDatabase.FormattingEnabled = true;
+            this.cboDatabase.Location = new System.Drawing.Point(142, 149);
+            this.cboDatabase.Name = "cboDatabase";
+            this.cboDatabase.Size = new System.Drawing.Size(384, 21);
+            this.cboDatabase.Sorted = true;
+            this.cboDatabase.TabIndex = 12;
+            this.cboDatabase.SelectedIndexChanged += new System.EventHandler(this.cboDatabase_SelectedIndexChanged);
+            // 
+            // lblDatabase
+            // 
+            this.lblDatabase.AutoSize = true;
+            this.lblDatabase.Location = new System.Drawing.Point(10, 152);
+            this.lblDatabase.Name = "lblDatabase";
+            this.lblDatabase.Size = new System.Drawing.Size(53, 13);
+            this.lblDatabase.TabIndex = 11;
+            this.lblDatabase.Text = "Database";
+            // 
+            // cmdLoadDBs
+            // 
+            this.cmdLoadDBs.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmdLoadDBs.Location = new System.Drawing.Point(409, 110);
+            this.cmdLoadDBs.Name = "cmdLoadDBs";
+            this.cmdLoadDBs.Size = new System.Drawing.Size(117, 23);
+            this.cmdLoadDBs.TabIndex = 10;
+            this.cmdLoadDBs.Text = "Load Databases";
+            this.cmdLoadDBs.UseVisualStyleBackColor = true;
+            this.cmdLoadDBs.Click += new System.EventHandler(this.cmdLoadDBs_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(215, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Sec";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sql server\\Instance";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(142, 87);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.Size = new System.Drawing.Size(384, 20);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // txtServer
+            // 
+            this.txtServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtServer.Location = new System.Drawing.Point(142, 12);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(384, 20);
+            this.txtServer.TabIndex = 1;
+            this.txtServer.TextChanged += new System.EventHandler(this.txtServer_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Password";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUserName.Location = new System.Drawing.Point(142, 61);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.ReadOnly = true;
+            this.txtUserName.Size = new System.Drawing.Size(384, 20);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Username";
+            // 
+            // chkIntegratedSec
+            // 
+            this.chkIntegratedSec.AutoSize = true;
+            this.chkIntegratedSec.Checked = true;
+            this.chkIntegratedSec.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkIntegratedSec.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.chkIntegratedSec.Location = new System.Drawing.Point(142, 38);
+            this.chkIntegratedSec.Name = "chkIntegratedSec";
+            this.chkIntegratedSec.Size = new System.Drawing.Size(111, 17);
+            this.chkIntegratedSec.TabIndex = 2;
+            this.chkIntegratedSec.Text = "Integrated security";
+            this.chkIntegratedSec.UseVisualStyleBackColor = true;
+            this.chkIntegratedSec.CheckedChanged += new System.EventHandler(this.chkIntegratedSec_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Time out value";
+            // 
+            // numericUpDownCmndTimeOut
+            // 
+            this.numericUpDownCmndTimeOut.Location = new System.Drawing.Point(142, 113);
+            this.numericUpDownCmndTimeOut.Maximum = new decimal(new int[] {
+            900,
+            0,
+            0,
+            0});
+            this.numericUpDownCmndTimeOut.Name = "numericUpDownCmndTimeOut";
+            this.numericUpDownCmndTimeOut.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDownCmndTimeOut.TabIndex = 8;
+            this.numericUpDownCmndTimeOut.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // SqlDatabaseSizeCollectorEditEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 288);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.cmdTest);
+            this.Controls.Add(this.cmdOK);
+            this.Controls.Add(this.cboDatabase);
+            this.Controls.Add(this.lblDatabase);
+            this.Controls.Add(this.cmdLoadDBs);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chkIntegratedSec);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDownCmndTimeOut);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SqlDatabaseSizeCollectorEditEntry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Edit Sql database size Entry";
+            this.Load += new System.EventHandler(this.SqlDatabaseSizeCollectorEditEntry_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.warningNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCmndTimeOut)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown warningNumericUpDown;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown errorNumericUpDown;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.Button cmdTest;
+        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.ComboBox cboDatabase;
+        private System.Windows.Forms.Label lblDatabase;
+        private System.Windows.Forms.Button cmdLoadDBs;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkIntegratedSec;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDownCmndTimeOut;
+    }
+}
---
+++ QuickMon4/QuickMonDBUIForWinForms/Collectors/SqlDatabaseSize/SqlDatabaseSizeCollectorUI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuickMon.UI;
+
+namespace QuickMon.Collectors
+{
+    public class SqlDatabaseSizeCollectorUI : WinFormsUICollectorBase
+    {
+        public override string AgentType { get { return "SqlDatabaseSizeCollector"; } }
+        public override ICollectorConfigEntryEditWindow DetailEditor { get { return new SqlDatabaseSizeCollectorEditEntry(); } }
+    }
+}
