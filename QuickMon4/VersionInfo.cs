cf77d90cbb85c626359b1ddc1a27ab25b5098e73
RudolfHenning_cp <SND\RudolfHenning_cp>
2016-03-03 06:47:01 +0000

    Version 4.2.303 (Beta)

--- QuickMon4/VersionInfo.cs
+++ QuickMon4/VersionInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Hen IT")]
 [assembly: AssemblyProduct("QuickMon")]
-[assembly: AssemblyCopyright("Copyright © Hen IT 2015")]
+[assembly: AssemblyCopyright("Copyright © Hen IT 2016")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.2.0.0226")]
-[assembly: AssemblyFileVersion("4.2.0.0226")]
+[assembly: AssemblyVersion("4.2.0.0303")]
+[assembly: AssemblyFileVersion("4.2.0.0303")]
