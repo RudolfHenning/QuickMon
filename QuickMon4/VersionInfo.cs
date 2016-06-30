584836a04cc61427199a6aee24eb315e4f378dc3
RudolfHenning_cp <SND\RudolfHenning_cp>
2016-06-30 08:02:44 +0000

    Version 4.3.5

--- QuickMon4/VersionInfo.cs
+++ QuickMon4/VersionInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.3.5.0629")]
-[assembly: AssemblyFileVersion("4.3.5.0629")]
+[assembly: AssemblyVersion("4.3.5.0701")]
+[assembly: AssemblyFileVersion("4.3.5.0701")]
