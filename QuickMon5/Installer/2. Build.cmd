97c35a9943f8d50ab9e2b5a9a66a975e18d0d7f3
Rudolf <rudolf.henning@gmail.com>
2017-04-25 15:13:51 +0200

    Alpha 2

--- QuickMon5/Installer/2. Build.cmd
+++ QuickMon5/Installer/2. Build.cmd
@@ -2,7 +2,7 @@ set WIXDIR=C:\Program Files (x86)\WiX Toolset v3.9\bin
 "%WIXDIR%\candle.exe" -out QuickMon5x64.winobj Productx64.wxs
 "%WIXDIR%\light.exe" -out QuickMon5x64.msi QuickMon5x64.winobj -ext WixUIExtension -ext WixUtilExtension -ext WixNetfxExtension
 "%WIXDIR%\candle.exe" -out QuickMon5.winobj Product.wxs
-"%WIXDIR%\light.exe" -out QuickMon5.msi QuickMon4.winobj -ext WixUIExtension -ext WixUtilExtension -ext WixNetfxExtension
+"%WIXDIR%\light.exe" -out QuickMon5.msi QuickMon5.winobj -ext WixUIExtension -ext WixUtilExtension -ext WixNetfxExtension
 
 powershell -File SetInstallerNames.ps1
 WaitOrKey.exe 5
