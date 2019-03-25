2f34711997e45a5ecc9d472847ab8d8f00db2eeb
Rudolf <Rudolf.Henning@gmail.com>
2019-03-25 21:18:25 +0200

    Adding GetFileVersion.ps1

---
+++ QuickMon5/Installer/Scripts/GetFileVersion.ps1
@@ -0,0 +1,27 @@
+﻿param(
+  [cmdletbinding()]
+  [parameter(position=0,mandatory=$true,valuefrompipeline=$true,ValueFromPipelineByPropertyName=$true)][string]$filePath
+)
+begin {
+}
+process {
+    $filePath = $filePath.Trim('"')
+    $versionInfo = ""
+
+    if (Test-Path $filePath){
+        $versionInfo = (Get-Command $filePath).FileVersionInfo.FileVersion
+    }
+    if ($versionInfo -eq ""){
+        "File not found!"
+    }
+    else {
+        $versionArray = $versionInfo.Split(".")
+        $major = [int]$versionArray[0]
+        $minor = [int]$versionArray[1]
+        $build = [int]$versionArray[2]
+        $str = $major.ToString() + "." + $minor.ToString() + "." + $build.ToString()
+        $str
+    }
+}
+end {
+}
