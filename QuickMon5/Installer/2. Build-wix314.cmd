9713f35d9c3c8f35ecaf0cee5b6c9b0114ba2823
RudolfHenning <Rudolf.Henning@gmail.com>
2024-04-06 19:08:54 +0200

    Add build batch file for Wix3.14

---
+++ QuickMon5/Installer/2. Build-wix314.cmd
@@ -0,0 +1,8 @@
+set WIXDIR=C:\Program Files (x86)\WiX Toolset v3.14\bin
+"%WIXDIR%\candle.exe" -out QuickMon5x64.winobj Productx64.wxs
+"%WIXDIR%\light.exe" -out QuickMon5x64.msi QuickMon5x64.winobj -ext WixUIExtension -ext WixUtilExtension -ext WixNetfxExtension
+"%WIXDIR%\candle.exe" -out QuickMon5.winobj Product.wxs
+"%WIXDIR%\light.exe" -out QuickMon5.msi QuickMon5.winobj -ext WixUIExtension -ext WixUtilExtension -ext WixNetfxExtension
+
+powershell -File SetInstallerNames.ps1
+WaitOrKey.exe 10
